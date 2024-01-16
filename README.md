21743e64fcf85053ea92f038ac9d962208859362
Henri Dewilde <henri.dewilde@gmail.com>
2024-01-16 17:49:18 +0100

    Vérification sur l'emprise

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ Pour cet exemple il s'agit de données de réseaux d'éclairage public (cables, 
 [x] Plusieurs cables ne doivent pas avoir la même géométrie  
 [x] Plusieurs supports / foyers / cables ne peuvent pas avoir le même nom  
 [x] Les câbles ne présentent pas d'auto-intersections, ni d'autre géométrie invalide  
-[] La couche emprise comprends un seul enregistrement  
-[] Tous les éléments supports, foyers et câbles sont à l'intérieur de l'emprise  
-[] Les champs de la couche emprise sont renseignés, le champ INSEE est numérique compris entre 10000 et 99999
+[x] La couche emprise comprends un seul enregistrement  
+[x] Tous les éléments supports, foyers et câbles sont à l'intérieur de l'emprise  
+[x] Les champs de la couche emprise sont renseignés, le champ INSEE est numérique compris entre 1000 et 99999
 
 
 Les vérifications effectuées ici sont données à titre d'exemple, des règles plus complèxes peuvent être envisagéees.  
--- sources/main.py
+++ sources/main.py
@@ -25,6 +25,10 @@ def verif_doublon_nom(data_set, data_set_name, data_set_name_field):
             if i > j and data_set[data_set_name_field][i] == data_set[data_set_name_field][j]:
                 print(f'les {data_set_name} {data_set[data_set_name_field][i]} et {data_set[data_set_name_field][j]} ont le même nom')
 
+def sont_inclus_dans_emprise(geom_emprise, data_set, data_set_name, data_set_name_field):
+    for i in data_set.index:
+        if not data_set['geometry'][i].within(geom_emprise):
+            print(f"Le {data_set_name} {data_set[data_set_name_field][i]} n'est pas situé à l'intérieur de l'emprise")
 
 path = os.path.realpath(__file__) 
 dir = os.path.dirname(path) 
@@ -33,6 +37,7 @@ dir_data = dir.replace('sources', 'test_data')
 supports = gpd.read_file(dir_data + "/supports.shp")
 foyers = gpd.read_file(dir_data + "/foyers.shp")
 cables = gpd.read_file(dir_data + "/cables.shp")
+emprise = gpd.read_file(dir_data + "/emprise.shp")
 
 # pour test si nom de type nn-nnn (ex: 01-234)
 regex = re.compile(r'\d{2}-\d{3}')
@@ -97,3 +102,21 @@ for i in supports.index:
             # On teste si le support est à l'une des deux extrémités
             if not supports['geometry'][i].equals(Point(cable_start_coords)) and not supports['geometry'][i].equals(Point(cable_end_coords)):
                 print(f"Le câble {cables['NOMCAB'][j]} n'est pas connecté au support de même nom")
+
+
+# Vérifications sur l'emprise 
+if len(emprise.index) != 1:
+    print("La couche emprise doit contenir un seul enregistrement")
+else:
+    # Vérifications attributaires
+    if emprise['NOM'][0] is None:
+        print("Le nom de l'emprise n'est pas renseigné")
+    if emprise['INSEE'][0] > 99999 or emprise['INSEE'][0] < 1000:
+        print(f"La valeur INSEE {emprise['INSEE'][0]} de l'emprise est incohérente")
+    if emprise['TECHNICIEN'][0] is None:
+        print("Le nom du technicien n'est pas renseigné dans l'emprise")
+
+    # Vérifications si les éléments supports, foyers, cables sont inclus dans la géométrie surfacique de l'emprise
+    sont_inclus_dans_emprise(emprise['geometry'][0], supports, "support", "NOMSUP")
+    sont_inclus_dans_emprise(emprise['geometry'][0], foyers, "foyer", "NOMFOY")
+    sont_inclus_dans_emprise(emprise['geometry'][0], cables, "câble", "NOMCAB")
